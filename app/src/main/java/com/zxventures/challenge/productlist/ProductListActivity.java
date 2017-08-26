3f5894c85ce4f5ab5ea677f73a1c05c22594ee0b
Leonardo Santos <leonardo.engelet@gmail.com>
2017-08-26 11:04:42 -0300

    Display loading state when displaying the `ProductListActivity` at first

--- app/src/main/java/com/zxventures/challenge/productlist/ProductListActivity.java
+++ app/src/main/java/com/zxventures/challenge/productlist/ProductListActivity.java
@@ -1,18 +1,29 @@
 package com.zxventures.challenge.productlist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ViewSwitcher;
 
 import com.zxventures.challenge.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ProductListActivity extends AppCompatActivity implements ProductListContract.ViewContract {
 
     ProductListContract.PresenterViewContract presenter;
 
+    @BindView(R.id.activity_product_list_viewswitcher)
+    ViewSwitcher viewSwitcher;
+    @BindView(R.id.activity_product_list_viewpager)
+    ViewPager categoriesViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+        ButterKnife.bind(this);
 
         ProductListInjector injector = ProductListInjector.newBuilder()
                 .build();
@@ -23,6 +34,6 @@ public class ProductListActivity extends AppCompatActivity implements ProductLis
 
     @Override
     public void showLoading() {
-        //Does nothing.
+        viewSwitcher.showNext();
     }
 }
